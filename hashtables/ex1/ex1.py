007d87d6c3beb20ae61d9eab4a1f3d6f610ca626
eralpkor <eralp.ny@gmail.com>
2020-06-07 13:59:40 -0400

    Finish merging two packages

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,21 @@
+
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    cache = {}
+
+    for i in range(length):
+        current_weight = weights[i]
+        # check if searched weight in cache, counterpart inserted previously
+        if current_weight in cache:
+            # get the index of previous weight
+            previously_seen_index = cache[current_weight]
+
+            return (i, previously_seen_index)
+
+        cache[limit - weights[i]] = i
 
     return None
+
+
+
+weights = [4, 6, 10, 15, 16]
+print(get_indices_of_item_weights(weights, 5, 21))
